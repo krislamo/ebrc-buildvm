eef5d0877cf78d75ac49d56648db498860e1fef3
Kris Lamoureux <krislamo@uga.edu>
2022-12-06 16:54:47 -0500

    Divide into options to decouple actions

--- buildvm.sh
+++ buildvm.sh
@@ -1,138 +1,226 @@
 #!/bin/bash
 
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+#################
+### Functions ###
+#################
+
 # Remove git alternatives reference
-cleanup_alt () {
-  if [[ -f .git/objects/info/alternates ]]; then
-    rm .git/objects/info/alternates
-  fi
+function cleanup_alt () {
+	if [ -f .git/objects/info/alternates ]; then
+		rm .git/objects/info/alternates
+	fi
 }
 
 # Function definition to checkout branch $VAGRANTENV if it isn't already there
-checkout_branch () {
-  cleanup_alt
-  CURRENTBRANCH=$(git status | awk 'NR==1{print $3}')
-  if [[ ! "$VAGRANTBRANCH" == "$CURRENTBRANCH" ]]; then
-    git checkout "$VAGRANTBRANCH"
-  fi
+function checkout_branch () {
+	local CURRENTBRANCH
+
+	cleanup_alt
+	CURRENTBRANCH=$(git status | awk 'NR==1{print $3}')
+	if [ ! "$VAGRANTBRANCH" == "$CURRENTBRANCH" ]; then
+		git checkout "$VAGRANTBRANCH"
+	fi
 }
 
-# The script starts here
-# Create environment directory
-mkdir -p "$PWD/env"
+# Restore vagrant virtual machine to previous snapshot
+function vagrant_restore () {
+	local RESPONSE
 
-# If argv[1] exists, set VAGRANTENV to it
-# RETURN 1 if file does not exist
-#
-# If argv[1] isn't set, set VAGRANTENV to 'main'
-# RETURN 1 if file does not exist
-if [[ ! -z ${1+x} ]]; then
-  if [[ -f "./env/$1" ]]; then
-    VAGRANTENV=$1
-  else
-    echo "FATAL: $PWD/env/$1 does not exist."
-    exit 1
-  fi
+	echo "You are about to restore '$VAGRANTBOX' to '$VAGRANTSNAP'"
+	read -r -p "Are you sure? [y/N] " RESPONSE
+	if [[ ! "$RESPONSE" =~ ^([yY])$ ]]; then
+		echo "FATAL: $USER declined to restore VM"
+		exit 1
+	fi
+}
+
+# Run commands or inline heredoc scripts
+function vagrant_run () {
+	ssh-add
+	vagrant ssh -c "$1"
+}
+
+# Run puppet apply inside the VM
+function puppet_apply () {
+	local SCRIPT
+
+	SCRIPT="sudo /opt/puppetlabs/bin/puppet apply --environment $VAGRANTBRANCH $PUPPETINIT"
+	vagrant_run "$SCRIPT"
+}
+
+# Symlink custom module list to /vagrant/scratch/<module>
+function puppet_relink () {
+	local COPY_MODULES
+	local SCRIPT
+
+	COPY_MODULES=$(for key in "${!MODULES[@]}"; do echo MODULES["$key"]=\""${MODULES[$key]}"\"; done)
+	SCRIPT=$(cat <<-END
+		set -xe;
+
+		cd /etc/puppetlabs/code/environments/$VAGRANTBRANCH &&
+		sudo rm -rf manifests Puppetfile data;
+		sudo ln -s /vagrant/scratch/puppet-control/manifests manifests;
+		sudo ln -s /vagrant/scratch/puppet-control/Puppetfile Puppetfile;
+		sudo ln -s /vagrant/scratch/puppet-hiera data;
+		declare -A MODULES
+		$COPY_MODULES
+		for mod in "\${!MODULES[@]}"
+		do
+		  sudo rm -rf modules/\${mod};
+		  sudo ln -s \${MODULES[\$mod]} modules/\${mod};
+		done
+	END
+	)
+	vagrant_run "$SCRIPT"
+}
+
+# r10k puppet deploy
+function puppet_deploy () {
+	local SCRIPT
+
+	SCRIPT=$(cat <<-END
+		set -xe;
+
+		GIT_HOST="$GIT_HOST";
+		GIT_PORT="$GIT_PORT";
+
+		if [ -n "\$GIT_HOST" ] && [ -n "\$GIT_PORT" ]; then
+			ssh-keyscan -p "\$GIT_PORT" "\$GIT_HOST" | sudo tee /root/.ssh/known_hosts
+		fi
+
+		sudo rm -rf /root/.r10k &&
+		sudo /usr/local/bin/r10k deploy environment $VAGRANTBRANCH -pv;
+	END
+	)
+	vagrant_run "$SCRIPT"
+}
+
+function vagrant_restore () {
+	local VMSTATE
+
+	VMSTATE=$(vboxmanage list runningvms | grep -c "$VAGRANTBOX")
+	[ ! "$VMSTATE" -eq 0 ] && vboxmanage controlvm "$VAGRANTBOX" poweroff
+	vboxmanage snapshot "$VAGRANTBOX" restore "$VAGRANTSNAP"
+	cd "$REPO"
+	vagrant up
+}
+
+################
+### Settings ###
+################
+
+# Clean environment
+unset APPLY
+unset DEPLOY
+unset ENV
+unset FROMSCRATCH
+unset HOSTINITPATH
+unset INITOVERRIDE
+unset LINKMODS
+unset PUPPETCONTROL
+unset PUPPETINIT
+unset REPO
+unset RESTORE
+unset VAGRANTBRANCH
+
+# Options
+while getopts ':ade:lr' OPTION; do
+	case "$OPTION" in
+		a) APPLY="true";;
+		d) DEPLOY="true";;
+		e) ENV="$OPTARG";;
+		l) LINKMODS="true";;
+		r) RESTORE="true";;
+		?)
+			echo "ERROR: Invalid option"
+			exit 1;;
+	esac
+done
+shift "$((OPTIND -1))"
+
+##########################
+### Changes start here ###
+##########################
+
+# Print commands and their arguments as they are executed
+set -x
+
+# Parameters
+REPO="$1"
+[ -z "$ENV" ] && ENV="main"
+
+# Check that REPO points to an actual git repository
+if [ ! -f "$REPO/.git/HEAD" ]; then
+	echo -e "ERROR: REPO '${REPO}' is not a git repository"
+	exit 1
+fi
+
+# Check for proper env directory and source it
+if [ ! -f "$REPO/scratch/build-puppetvm/env/$ENV" ]; then
+	echo "ERROR: ENV file $REPO/scratch/build-puppetvm/env/$ENV does not exist"
+	exit 1
+fi
+
+# Source dynamic configuration plus additional settings
+# shellcheck source=/dev/null
+source "$REPO/scratch/build-puppetvm/env/$ENV"
+
+# Check for $VAGRANTBRANCH variable
+if [ -z "$VAGRANTBRANCH" ]; then
+	echo "ERROR: VAGRANTBRANCH is undefined"
+	exit 1
 else
-  if [[ -f "./env/main" ]]; then
-    VAGRANTENV=main
-  else
-    echo "FATAL: default 'main' environment does not exist"
-    exit 1
-  fi
+	PUPPETINIT="/etc/puppetlabs/code/environments/$VAGRANTBRANCH/manifests"
 fi
 
-# Source environment
-. "$PWD/env/$VAGRANTENV"
+# Validate FROMSCRATCH setting
+[ -n "$FROMSCRATCH" ] &&
+	if [ ! -d "$FROMSCRATCH" ]; then
+		echo "ERROR: '$FROMSCRATCH' does not exist"
+		exit 1
+	fi
 
-# Default puppet-apply manifest location on the virtual machine
-PUPPETINIT="/etc/puppetlabs/code/environments/$VAGRANTBRANCH/manifests"
+if [ -n "$INITOVERRIDE" ]; then
+	HOSTINITPATH="$REPO/scratch/puppet-control/manifests/$INITOVERRIDE"
 
-# Prompt user about destructive virtual machine restore
-# RETURN 1 on decline
-echo "You are about to restore '$VAGRANTBOX' to '$VAGRANTSNAP'"
-read -r -p "Are you sure? [y/N] " RESPONSE
-if [[ ! "$RESPONSE" =~ ^([yY])$ ]]; then
-  echo "FATAL: $USER declined to restore VM"
-  exit 1
+	if [ ! -f "$HOSTINITPATH/$ENV-init.txt" ]; then
+		echo "ERROR: $ENV-init.txt override not found"
+		exit 1
+	else
+		rm -rf "$HOSTINITPATH/init.pp"
+		cp "$HOSTINITPATH/$ENV-init.txt" "$HOSTINITPATH/init.pp"
+	fi
+else
+	PUPPETINIT="$PUPPETINIT/site.pp"
 fi
-
-# System changes start here
-# Stop on failures and output commands to terminal
-set -xe
 
 # Optional scratch directory sync
-if [[ ! -z ${FROMSCRATCH+x} ]]; then
-  rsync -avr -H --delete "$FROMSCRATCH" "$VAGRANTDIR/scratch"
+if [ -n "$FROMSCRATCH" ]; then
+	rsync -avr -H --delete "$FROMSCRATCH" "$REPO/scratch"
 fi
 
 # puppet-control repository checkout
-PUPPETCONTROL="$VAGRANTDIR/scratch/puppet-control"
+PUPPETCONTROL="$REPO/scratch/puppet-control"
 cd "$PUPPETCONTROL"
 checkout_branch
 
-# Optional init.pp manifest override
-# Fall back to using puppet-control/manifests/site.pp for savm
-if [[ ! -z ${INITOVERRIDE+x} ]]; then
-  HOSTINITPATH="$PUPPETCONTROL/manifests/$INITOVERRIDE"
-  rm -rf "$HOSTINITPATH/init.pp"
-  cp "$HOSTINITPATH/$VAGRANTENV-init.txt" "$HOSTINITPATH/init.pp"
-else
-  PUPPETINIT="$PUPPETINIT/site.pp"
-fi
-
 # puppet-hiera respository checkout
-cd "$VAGRANTDIR/scratch/puppet-hiera"
+cd "$REPO/scratch/puppet-hiera"
 checkout_branch
 
 # puppet-profiles repository checkout, rebasing on master
-cd "$VAGRANTDIR/scratch/puppet-profiles"
+cd "$REPO/scratch/puppet-profiles"
 cleanup_alt
 git checkout master
 git pull
-git checkout $VAGRANTBRANCH
+git checkout "$VAGRANTBRANCH"
 git rebase master
 
-# Reset VirtualBox state
-VAGRANTBOXSTATE=$(vboxmanage list runningvms | grep "$VAGRANTBOX" | wc -l)
-[[ ! "$VAGRANTBOXSTATE" -eq 0 ]] && vboxmanage controlvm "$VAGRANTBOX" poweroff
-vboxmanage snapshot "$VAGRANTBOX" restore "$VAGRANTSNAP"
-
-# Apply Puppet to virtual machine
-COPY_MODULES=$(for key in "${!MODULES[@]}"; do echo MODULES[$key]=\"${MODULES[$key]}\"; done)
-SCRIPT=$(cat <<-END
-  set -xe;
-
-  GIT_HOST="$GIT_HOST";
-  GIT_PORT="$GIT_PORT";
-  if [[ ! -z \$GIT_HOST ]] && [[ ! -z \$GIT_PORT ]]; then
-    ssh-keyscan -p \$GIT_PORT \$GIT_HOST | sudo tee /root/.ssh/known_hosts
-  fi
-
-  sudo rm -rf /root/.r10k &&
-  sudo /usr/local/bin/r10k deploy environment $VAGRANTBRANCH -pv;
-
-  cd /etc/puppetlabs/code/environments/$VAGRANTBRANCH &&
-  sudo rm -rf manifests Puppetfile data;
-  sudo ln -s /vagrant/scratch/puppet-control/manifests manifests;
-  sudo ln -s /vagrant/scratch/puppet-control/Puppetfile Puppetfile;
-  sudo ln -s /vagrant/scratch/puppet-hiera data;
-
-  declare -A MODULES
-  $COPY_MODULES
-
-  for mod in "\${!MODULES[@]}"
-  do
-    sudo rm -rf modules/\${mod};
-    sudo ln -s \${MODULES[\$mod]} modules/\${mod};
-  done
-
-  sudo /opt/puppetlabs/bin/puppet apply --environment $VAGRANTBRANCH $PUPPETINIT;
-  sudo -i;
-END
-)
-
-cd "$VAGRANTDIR"
-vagrant up
-sleep 2
-ssh-add
-vagrant ssh -c "$SCRIPT"
+# Run/apply vagrant/puppet options
+[ "$RESTORE" == "true" ] && vagrant_restore
+[ "$DEPLOY" == "true" ] && puppet_deploy
+[ "$LINKMODS" == "true" ] && puppet_relink
+[ "$APPLY" == "true" ] && puppet_apply
